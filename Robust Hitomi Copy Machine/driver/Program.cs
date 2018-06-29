0c333f879e089df98887dc0a1d91a4d240cf864e
dc-koromo <jumpwin613@naver.com>
2018-06-29 10:08:57 +0900

    [Hitomi 3] Fix driver for MM

--- Robust Hitomi Copy Machine/driver/Program.cs
+++ Robust Hitomi Copy Machine/driver/Program.cs
@@ -23,6 +23,7 @@ namespace driver
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(args[1]);
             request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
             request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36";
+            if (args[1].Contains("wasabisyrup.com")) request.Referer = "http://wasabisyrup.com/archives/";
 
             request.Timeout = Timeout.Infinite;
             request.KeepAlive = true;
