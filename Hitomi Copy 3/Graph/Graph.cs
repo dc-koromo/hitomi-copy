78f2dff68cc22ad3d20d2843b3f8cd9559243a2d
dc-koromo <jumpwin613@naver.com>
2018-06-30 10:41:34 +0900

    [Hitomi 3] Add Graph Prototype

---
+++ Hitomi Copy 3/Graph/Graph.cs
@@ -0,0 +1,92 @@
+﻿/* Copyright (C) 2018. Hitomi Parser Developers */
+
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Hitomi_Copy_3.Graph
+{
+    public partial class Graph : UserControl
+    {
+        ViewManager vm;
+        float zoom = 1.0F;
+
+        public Graph()
+        {
+            InitializeComponent();
+
+            MouseDown += new MouseEventHandler(OnMouseDown);
+            MouseUp += new MouseEventHandler(OnMouseUp);
+            MouseMove += new MouseEventHandler(OnMouseMove);
+            MouseClick += new MouseEventHandler(OnMouseClick);
+            MouseWheel += new MouseEventHandler(OnMouseWheel);
+
+            vm = new ViewManager(Font);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            vm.Render(e.Graphics, this.Size, PointToClient(Cursor.Position), zoom);
+            base.OnPaint(e);
+        }
+
+        #region Mouse Event
+
+        bool ml_down = false;
+        bool mr_down = false;
+        Point mr_pos;
+        Point ml_pos;
+
+        private void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            if (ml_down == false && e.Button == MouseButtons.Left)
+            {
+                ml_down = true;
+                ml_pos = e.Location;
+
+                Invalidate();
+            }
+        }
+        private void OnMouseUp(object sender, MouseEventArgs e)
+        {
+            if (ml_down == true)
+            {
+                ml_down = false;
+                
+                Invalidate();
+            }
+            else if (mr_down == true)
+            {
+                mr_down = false;
+            }
+        }
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (ml_down)
+            {
+                Cursor = Cursors.Hand;
+                
+                int dx = ml_pos.X - e.Location.X;
+                int dy = ml_pos.Y - e.Location.Y;
+
+                vm.Move((int)(dx / zoom), (int)(dy / zoom));
+
+                ml_pos = e.Location;
+                Invalidate();
+            }
+            else if (mr_down)
+            {
+                mr_pos = e.Location;
+
+                Invalidate();
+            }
+        }
+        private void OnMouseClick(object sender, MouseEventArgs e)
+        {
+        }
+        private void OnMouseWheel(object sender, MouseEventArgs e)
+        {
+        }
+        
+        #endregion
+    }
+}
---
+++ Hitomi Copy 3/Graph/ViewManager.cs
@@ -0,0 +1,33 @@
+﻿/* Copyright (C) 2018. Hitomi Parser Developers */
+
+using System.Drawing;
+
+namespace Hitomi_Copy_3.Graph
+{
+    public class ViewManager
+    {
+        Point bp;
+        ViewBuffer vb;
+        Font font;
+
+        public ViewManager(Font font)
+        {
+            vb = new ViewBuffer();
+            bp = new Point(0, 0);
+
+            this.font = font;
+        }
+
+        public void Render(Graphics g, Size sizeOfPannel, Point mousePosition, float scale = 1.0F)
+        {
+            vb.CreateGraphics(sizeOfPannel.Width, sizeOfPannel.Height);
+
+            vb.Draw(g);
+        }
+
+        public void Move(int dx, int dy)
+        {
+            bp = new Point(bp.X - dx, bp.Y - dy);
+        }
+    }
+}
