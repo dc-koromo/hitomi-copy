6a37081e8f07c966402e3523dc04f34c22dfb91b
dc-koromo <jumpwin613@naver.com>
2018-07-01 20:57:35 +0900

    [Hitomi 3] Add MMUpdateChecker Prototype

---
+++ Hitomi Copy 3/MM/MMUpdateChecker.cs
@@ -0,0 +1,18 @@
+﻿/* Copyright (C) 2018. Hitomi Parser Developers */
+
+using System;
+using System.Windows.Forms;
+
+namespace Hitomi_Copy_3.MM
+{
+    public class MMUpdateChecker
+    {
+        private static readonly Lazy<MMUpdateChecker> instance = new Lazy<MMUpdateChecker>(() => new MMUpdateChecker());
+        public static MMUpdateChecker Instance => instance.Value;
+
+        private void ShowMarumaruManager()
+        {
+            Application.OpenForms[0].Post(() => (new frmMarumaru()).Show());
+        }
+    }
+}
--- Hitomi Copy 3/frmMarumaru.Designer.cs
+++ Hitomi Copy 3/frmMarumaru.Designer.cs
@@ -29,14 +29,50 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMarumaru));
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 27);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(405, 400);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(259, 433);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "갱신하기";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "다음의 마루마루 만화를 갱신할 수 있습니다!";
             // 
             // frmMarumaru
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(429, 497);
+            this.ClientSize = new System.Drawing.Size(429, 468);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
             this.Font = new System.Drawing.Font("맑은 고딕", 9F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -46,9 +82,14 @@
             this.Name = "frmMarumaru";
             this.Text = "Marumaru manager";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
