0a8c8fbc2979f25c8b26cad564ae6065ba0c8151
dc-koromo <jumpwin613@naver.com>
2018-07-02 15:58:52 +0900

    [Hitomi 3] Add estimateting remain time

--- Hitomi Copy 3/403/403Tester.Designer.cs
+++ Hitomi Copy 3/403/403Tester.Designer.cs
@@ -34,6 +34,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -79,12 +81,32 @@
             this.label3.Size = new System.Drawing.Size(0, 15);
             this.label3.TabIndex = 4;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(204, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "남은 시간 : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(280, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "0";
+            // 
             // _403Tester
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(610, 125);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button1);
@@ -111,5 +133,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
--- Hitomi Copy 3/403/403Tester.cs
+++ Hitomi Copy 3/403/403Tester.cs
@@ -23,6 +23,7 @@ namespace Hitomi_Copy_3._403
         List<int> magics = new List<int>();
         HashSet<int> magic_set = new HashSet<int>();
         List<HitomiArticle> new_article = new List<HitomiArticle>();
+        int status = 0;
         private void _403Tester_Load(object sender, System.EventArgs e)
         {
             foreach (var metadata in HitomiData.Instance.metadata_collection)
@@ -35,48 +36,63 @@ namespace Hitomi_Copy_3._403
             progressBar1.Maximum = magics.Count;
         }
 
-        private async void button1_ClickAsync(object sender, System.EventArgs e)
+        DateTime start;
+        private void button1_ClickAsync(object sender, System.EventArgs e)
         {
-            await Task.WhenAll(Enumerable.Range(0, 30).Select(no => Task.Run(() => process(no))));
-            File.WriteAllText("403.json", LogEssential.SerializeObject(new_article));
+            //await Task.WhenAll(Enumerable.Range(0, 30).Select(no => Task.Run(() => process(no))));
+            start = DateTime.Now;
+            for (int i = 0; i < 30; i++)
+            {
+                Notify();
+            }
         }
 
         private void process(int i)
         {
-            int min = magics.Count / 30 * i;
-            int max = magics.Count / 30 * (i + 1);
-            if (max > magics.Count)
-                max = magics.Count;
-
-            for (int j = min; j < max; j++)
+            try
             {
-                try
+                WebClient wc = new WebClient();
+                wc.Encoding = Encoding.UTF8;
+                string x;
+                x = wc.DownloadString("https://hitomi.la/galleries/" + magics[i] + ".html");
+                var list = HitomiParser.ParseArticles(x);
+                foreach (var data in list)
                 {
-                    WebClient wc = new WebClient();
-                    wc.Encoding = Encoding.UTF8;
-                    string x;
-                    x = wc.DownloadString("https://hitomi.la/galleries/" + magics[j] + ".html");
-                    var list = HitomiParser.ParseArticles(x);
-                    foreach (var data in list)
+                    if (!magic_set.Contains(Convert.ToInt32(data.Magic)))
                     {
-                        if (!magic_set.Contains(Convert.ToInt32(data.Magic)))
-                        {
-                            magic_set.Add(Convert.ToInt32(data.Magic));
-                            new_article.Add(data);
-                            LogEssential.Instance.PushLog(() => $"New! {j}");
-                        }
+                        magic_set.Add(Convert.ToInt32(data.Magic));
+                        new_article.Add(data);
+                        LogEssential.Instance.PushLog(() => $"New! {data.Magic}");
                     }
                 }
-                catch (Exception ex)
-                {
-                    LogEssential.Instance.PushLog(() => $"{j} error {ex.Message}");
-                }
-
-                this.Post(() => progressBar1.Value++);
-                this.Post(() => label3.Text = $"{progressBar1.Value}/{magics.Count} 분석완료");
+            }
+            catch (Exception ex)
+            {
+                LogEssential.Instance.PushLog(() => $"{i} error {ex.Message}");
             }
 
-            LogEssential.Instance.PushLog(() => $"{min}/{max} process start!");
+            this.Post(() => progressBar1.Value++);
+            this.Post(() => label3.Text = $"{progressBar1.Value}/{magics.Count} 분석완료");
+            this.Post(() => label5.Text = $"{(new DateTime((DateTime.Now - start).Ticks * magics.Count / progressBar1.Value)).ToString("HH시간 mm분 ss초")}");
+            
+            lock (int_lock) mtx--;
+            lock (notify_lock) Notify();
         }
+
+        private void Notify()
+        {
+            lock (int_lock)
+            {
+                if (status < magics.Count) { Task.Run(() => process(status)); status++; mtx++; }
+                if (status >= magics.Count && mtx == 0)
+                    lock (new_article) File.WriteAllText("403.json", LogEssential.SerializeObject(new_article));
+            }
+        }
+
+        int mtx = 0;
+
+        object int_lock = new object();
+        object notify_lock = new object();
+        
     }
 }
