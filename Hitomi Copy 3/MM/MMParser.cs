65af3c79daab099f1ade7ccc051fed8c6b591ace
dc-koromo <jumpwin613@naver.com>
2018-07-04 14:59:09 +0900

    [Hitomi 3] Fix MM thumbnail parse rule

--- Hitomi Copy 3/MM/MMParser.cs
+++ Hitomi Copy 3/MM/MMParser.cs
@@ -47,8 +47,8 @@ namespace MM_Downloader.MM
         /// <returns></returns>
         public static string GetThumbnailAddress(string html)
         {
-            string thumbnail = Regex.Split(Regex.Split(html, "https://marumaru.in/quickimage/")[1], @".jpg")[0];
-            return "https://marumaru.in/quickimage/" + thumbnail + ".jpg";
+            string thumbnail = Regex.Split(Regex.Split(html, "https://marumaru.in/quickimage/")[1], "\"")[0];
+            return "https://marumaru.in/quickimage/" + thumbnail;
         }
 
         /// <summary>
