a711ff817bfbd7c599100fc0c76aed62b70ee9c1
dc-koromo <koromo.software@gmail.com>
2018-07-20 10:09:25 +0900

    [Hitomi 3] Add CustomArtistRecommendation prototype

---
+++ Hitomi Copy 3/CustomArtistRecommendation.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Hitomi_Copy_3
+{
+    partial class CustomArtistRecommendation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CustomArtistRecommendation));
+            this.lvCustomTag = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvArtists = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lvCustomTag
+            // 
+            this.lvCustomTag.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.lvCustomTag.FullRowSelect = true;
+            this.lvCustomTag.GridLines = true;
+            this.lvCustomTag.Location = new System.Drawing.Point(12, 12);
+            this.lvCustomTag.Name = "lvCustomTag";
+            this.lvCustomTag.Size = new System.Drawing.Size(246, 459);
+            this.lvCustomTag.TabIndex = 0;
+            this.lvCustomTag.UseCompatibleStateImageBehavior = false;
+            this.lvCustomTag.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "태그";
+            this.columnHeader1.Width = 139;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "점수";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader2.Width = 74;
+            // 
+            // lvArtists
+            // 
+            this.lvArtists.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.lvArtists.FullRowSelect = true;
+            this.lvArtists.GridLines = true;
+            this.lvArtists.Location = new System.Drawing.Point(264, 12);
+            this.lvArtists.Name = "lvArtists";
+            this.lvArtists.Size = new System.Drawing.Size(817, 459);
+            this.lvArtists.TabIndex = 1;
+            this.lvArtists.UseCompatibleStateImageBehavior = false;
+            this.lvArtists.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "순위";
+            this.columnHeader3.Width = 59;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "작가";
+            this.columnHeader4.Width = 122;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "점수";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader5.Width = 91;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "태그";
+            this.columnHeader6.Width = 509;
+            // 
+            // CustomArtistRecommendation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1093, 483);
+            this.Controls.Add(this.lvArtists);
+            this.Controls.Add(this.lvCustomTag);
+            this.Font = new System.Drawing.Font("맑은 고딕", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CustomArtistRecommendation";
+            this.Text = "Custom Artist Recommendation";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvCustomTag;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ListView lvArtists;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+    }
+}
