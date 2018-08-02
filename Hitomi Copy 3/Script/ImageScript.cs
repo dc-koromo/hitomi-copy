117e2c2169cad58b9ac3cd36f831e54f80f02334
dc-koromo <koromo.software@gmail.com>
2018-08-03 00:38:15 +0900

    [Hitomi 3] Add ImageScript Prototype

---
+++ Hitomi Copy 3/Script/ImageScript.cs
@@ -0,0 +1,20 @@
+﻿/* Copyright (C) 2018. Hitomi Parser Developers */
+
+namespace Hitomi_Copy_3.Script
+{
+    public class ImageScriptModel
+    {
+    }
+
+    public class ImageScriptParser
+    {
+    }
+
+    public class ImageScriptBuilder
+    {
+    }
+
+    public class ImageScript
+    {
+    }
+}
