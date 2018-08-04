fbdb69715ef16ee552cfe151e8230ad1d0503929
dc-koromo <koromo.software@gmail.com>
2018-08-04 20:44:29 +0900

    [Hitomi 3] Update Version

--- Hitomi Copy 3/PatchNote.Designer.cs
+++ Hitomi Copy 3/PatchNote.Designer.cs
@@ -61,7 +61,7 @@
             this.MinimumSize = new System.Drawing.Size(459, 474);
             this.Name = "PatchNote";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Patch Note v3.41";
+            this.Text = "Patch Note v3.42";
             this.Load += new System.EventHandler(this.PatchNote_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
